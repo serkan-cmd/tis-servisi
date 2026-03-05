ecc9beaba006f8e6105723353aa0a5ed9f3c5082
serkan-cmd <serkangumusbas@petrol-is.org.tr>
2026-03-05 10:57:53 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ import streamlit as st
 import pandas as pd
 
 # Sayfa ayarları
-st.set_page_config(page_title="TİS Servisi v1.0", layout="wide")
+st.set_page_config(page_title="Petrol-İş TİS Servisi v1.0", layout="wide")
 
-st.title("📊 TİS Maliyet Hesaplama Platformu")
+st.title("📊 Petrol-İş TİS Servisi")
 st.markdown("---")
 
 # Yan Menü: Sabit Parametreler
